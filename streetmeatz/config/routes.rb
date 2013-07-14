0f0c03b0827fcd0a96b344b36fa6c92e04401ded
Anuj Jhunjhunwala <anuj.jhunjhunwala@gmail.com>
2013-07-14 19:13:05 -0400

    Moved wireframe files into wireframe folder

--- streetmeatz/config/routes.rb
+++ streetmeatz/config/routes.rb
@@ -1,3 +1,8 @@
 Streetmeatz::Application.routes.draw do
   root :to => 'welcome#index'
+
+  # Other routes will include:
+  # streetcarts (CRUD)
+  # menuitems (CRUD)
+  # reviews (just to add reviews)
 end
