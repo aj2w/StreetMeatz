e1340febe524f1f0c4ffd3728a09fd60f88082a1
Anuj Jhunjhunwala <anuj.jhunjhunwala@gmail.com>
2013-07-14 19:04:31 -0400

    Rails app folder created, first commit

---
+++ streetmeatz/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Streetmeatz::Application.config.secret_token = '1be507a027dd054e7986b8346a676e52727a83d5750030f1871c530e29f160b1604164e5751adf9f2a3daf8a2cf7aab4deeb4480cae649460464f1d09c69a7fe'
---
+++ streetmeatz/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Streetmeatz::Application.config.session_store :cookie_store, key: '_streetmeatz_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Streetmeatz::Application.config.session_store :active_record_store
