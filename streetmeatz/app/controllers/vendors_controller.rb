4235ce5536a0d9c90f93251337981e16bcb9b201
Anuj Jhunjhunwala <anuj.jhunjhunwala@gmail.com>
2013-07-15 17:21:54 -0400

    Created edit page for vendors, allowed for dynamic location updating

--- streetmeatz/app/controllers/vendors_controller.rb
+++ streetmeatz/app/controllers/vendors_controller.rb
@@ -12,6 +12,44 @@ class VendorsController < ApplicationController
   end
 
   def new
+    # name = params[:name]
+    # address = params[:address]
+    # hours = params[:hours]
+    # phone = params[:phone]
+    # twitter = params[:twitter]
     @vendor = Vendor.new
+  end
+
+  def create
+    vendor = Vendor.new
+    vendor.name = params[:name]
+    vendor.address = params[:address]
+    vendor.hours = params[:hours]
+    vendor.phone = params[:phone]
+    vendor.twitter = params[:twitter]
+    vendor.save
+    redirect_to('/vendors')
+  end
+
+  def edit
+    @vendor = Vendor.find(params[:id])
+    name = params[:name]
+    address = params[:address]
+    hours = params[:hours]
+    phone = params[:phone]
+    twitter = params[:twitter]
+  end
+
+  def update
+    vendor = Vendor.find(params[:id])
+    vendor.name = params[:name]
+    vendor.address = params[:address]
+    vendor.hours = params[:hours]
+    vendor.phone = params[:phone]
+    vendor.twitter = params[:twitter]
+    vendor.save
+    redirect_to "/vendors/#{vendor.id}"
+  end
+
 
 end
--- streetmeatz/app/models/vendor.rb
+++ streetmeatz/app/models/vendor.rb
@@ -1,7 +1,7 @@
 class Vendor < ActiveRecord::Base
 # Inserts relational aspects here
 
-  acts_as_gmappable
+  acts_as_gmappable :check_process => false
 
   def gmaps4rails_address
     address
