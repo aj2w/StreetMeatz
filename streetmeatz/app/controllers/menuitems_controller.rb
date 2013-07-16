43fed91ac24b6b04791b5c3b9600acee0f32ca62
Anuj Jhunjhunwala <anuj.jhunjhunwala@gmail.com>
2013-07-16 15:25:04 -0400

    Fixed new/edit logic for menuitems

--- streetmeatz/app/controllers/menuitems_controller.rb
+++ streetmeatz/app/controllers/menuitems_controller.rb
@@ -12,43 +12,28 @@ class MenuitemsController < ApplicationController
     @menuitem = Menuitem.new
   end
 
-  #  def create
-  #   student = Student.new(params[:student])
-  #   student.save
-  #   redirect_to @student
-  # end
-
   def create
     menuitem = Menuitem.new(params[:menuitem])
-
-    # menuitem.name = params[:name]
-    # menuitem.description = params[:description]
-    # menuitem.price = params[:price]
-    # menuitem.image = params[:image]
     menuitem.save
-
-    # vendor = Vendor.find(params[:vendor])
-    # vendor.menuitems << menuitem
-    redirect_to('/menuitems')
+    redirect_to '/menuitems'
   end
+
 
   def edit
     @menuitem = Menuitem.find(params[:id])
-    @vendors = Vendor.all
-    name = params[:name]
-    description = params[:description]
-    price = params[:price]
-    image = params[:image]
   end
 
+  # def update
+  #   @student = Student.find(params[:id])
+  #   raise
+  #   @student.update_attributes(params[:student])
+  #   redirect_to @student
+  # end
+
   def update
-    menuitem = Menuitem.find(params[:id])
-    menuitem.name = params[:name]
-    menuitem.description = params[:description]
-    menuitem.price = params[:price]
-    menuitem.image = params[:image]
-    menuitem.save
-    redirect_to "menuitems/#{menuitem.id}"
+    @menuitem = Menuitem.find(params[:id])
+    @menuitem.update_attributes(params[:menuitem])
+    redirect_to @menuitem
   end
 
   def destroy
