6f345db47c40eab5fcedf4b1c25cafac5e6a648b
Anuj Jhunjhunwala <anuj.jhunjhunwala@gmail.com>
2013-10-18 22:47:36 -0400

    trying time-series points

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -10,7 +10,10 @@ def index
     statsd = Statsd.new
     statsd.increment('web.page_views')
     dog = Dogapi::Client.new(api_key, application_key)
-    dog.emit_point('web.page_views', 1.0, :host => "streetsidebites.herokuapp.com")
+
+    points = [[Time.now, 0], [Time.now + 10, 10.0], [Time.now + 20, 20.0]]
+    dog.emit_points('web.page_views', points, tags => ["version:1"])
+    # dog.emit_point('web.page_views', 1.0, :host => "streetsidebites.herokuapp.com")
 
 
     @json = Vendor.all.to_gmaps4rails
